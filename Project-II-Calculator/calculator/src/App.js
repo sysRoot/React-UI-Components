86b9a52a6096fe61a4cf1ec946f608cb536621d1
Jason Cook <the.root.user@gmail.com>
2019-03-04 02:22:31 -0500

    Make calculator functions operate

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,44 +1,200 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import update from "immutability-helper";
 import "./App.css";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
 import ActionButton from "./components/ButtonComponents/ActionButton";
 import NumberButton from "./components/ButtonComponents/NumberButton";
+import math from "mathjs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      calcDisp: 0,
+      operations: []
+    };
+  }
+
+  calculateOperations = () => {
+    let result = this.state.operations.join('')
+    if (result) {
+      result = math.eval(result)
+      result = math.format(result, { precision: 14 })
+      this.setState({
+        operations: [result],
+      })
     }
   }
 
-  onClickButton() {
+  onClickButton = e => {
+    const value = e.target.getAttribute('data-value')
+    switch (value) {
+      case 'clear':
+        this.setState({
+          operations: [],
+        })
+        break
+      case '=':
+        this.calculateOperations()
+        break
+      default:
+        const newOperations = update(this.state.operations, {
+          $push: [value],
+        })
+        this.setState({
+          operations: newOperations,
+        })
+        break
+    }
+  };
 
+  keyPress = e => {
+    const value = e.key;
+
+    const allowed = [
+      '1', '2', '3', '4',
+      '5', '6', '7', '8',
+      '9', '0', '+', '-', '*', '/', '=', '.',
+      'Enter', 'equal', 'Escape', 'C',
+      'Backspace'
+    ];
+
+    if (allowed.includes(value)) {
+      switch (value) {
+        case 'Escape':
+          this.setState({
+            operations: [],
+          })
+          break
+        case 'Backspace':
+          const backSpaceOperations = this.state.operations;
+          backSpaceOperations.pop()
+          this.setState({
+            operations: backSpaceOperations,
+          })
+          break
+        case '=':
+        case 'Enter':
+          this.calculateOperations()
+          break
+        default:
+          const newOperations = update(this.state.operations, {
+            $push: [value],
+          })
+          this.setState({
+            operations: newOperations,
+          })
+          break
+      }
+    }
   }
 
-  render() { 
+  componentWillMount = (e) => {
+    document.addEventListener("keydown", this.keyPress.bind(this));
+  }
+
+  render() {
     return (
       <div className="container">
-      <CalculatorDisplay display={this.state.calcDisp}/>
-      <ActionButton styling="button-action" text="clear" onClick="" />
-      <NumberButton styling="button-number" type="operators" text="&#xf7;" onClick="" />
-      <NumberButton styling="button-number" weight="bold" text="7" onClick="" />
-      <NumberButton styling="button-number" weight="bold" text="8" onClick=""/>
-      <NumberButton styling="button-number" weight="bold" text="9" />
-      <NumberButton styling="button-number" type="operators" text="&#xd7;" />
-      <NumberButton styling="button-number" weight="bold" text="4" />
-      <NumberButton styling="button-number" weight="bold" text="5" />
-      <NumberButton styling="button-number" weight="bold" text="6" />
-      <NumberButton styling="button-number" type="operators" text="&#x2212;" />
-      <NumberButton styling="button-number" weight="bold" text="1" />
-      <NumberButton styling="button-number" weight="bold" text="2" />
-      <NumberButton styling="button-number" weight="bold" text="3" />
-      <NumberButton styling="button-number" type="operators" text="&#x2b;" />
-      <ActionButton styling="button-action" weight="bold" text="0" />
-      <NumberButton styling="button-number" type="operators" text="&#x3d;" />
-    </div>
+        <CalculatorDisplay display={this.state.operations} />
+        <ActionButton
+          styling="button-action"
+          text="clear"
+          clicker={this.onClickButton}
+          kpress={this.keyPress}
+        />
+        <NumberButton
+          styling="button-number"
+          type="operators"
+          text="&#47;"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="7"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="8"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="9"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          type="operators"
+          text="&#42;"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="4"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="5"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="6"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          type="operators"
+          text="&#x2212;"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="1"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="2"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          weight="bold"
+          text="3"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          type="operators"
+          text="&#x2b;"
+          clicker={this.onClickButton}
+        />
+        <ActionButton
+          styling="button-action"
+          weight="bold"
+          text="0"
+          clicker={this.onClickButton}
+        />
+        <NumberButton
+          styling="button-number"
+          type="operators"
+          text="&#x3d;"
+          clicker={this.onClickButton}
+        />
+      </div>
     );
   }
 }
- 
+
 export default App;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -3,7 +3,14 @@ import "./Button.css";
 
 const ActionButton = props => {
   return (
-    <div className={`${props.styling} ${props.weight} ${props.text}`}>{props.text}</div>
+    <div
+      className={`${props.styling} ${props.weight} ${props.text}`}
+      onClick={props.clicker}
+      onKeyPress={props.kPress}
+      data-value={props.text}
+    >
+      {props.text}
+    </div>
   );
 };
 
